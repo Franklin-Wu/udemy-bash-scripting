24ac8e0481f26121f1c56b2b04557fc6b9857989
Franklin Wu <franklin.w.wu@gmail.com>
2018-08-02 01:19:51 -0700

    Complete through 08-logging

--- exercises/08-logging/exercise-01.sh
+++ exercises/08-logging/exercise-01.sh
@@ -5,3 +5,10 @@
 # messages.
 #
 # Hint: Use $RANDOM
+
+NUMBER=$RANDOM
+echo "${NUMBER}"
+logger -p user.info -t "$0" "${NUMBER}"
+
+# The following works on MacOS, where looking in /var/log/messages or /var/log/sysinfo does not.
+log show --predicate "eventMessage contains '${NUMBER}'" --last 10m --info
--- exercises/08-logging/exercise-02.sh
+++ exercises/08-logging/exercise-02.sh
@@ -2,3 +2,18 @@
 
 # Modify the previous script so that it uses a logging function. Additionally tag each syslog
 # message with "randomly" and include the process ID. Generate 3 random numbers.
+
+function logit() {
+	local LOG_LEVEL=$1
+	shift
+	local MESSAGE=$@
+	logger -i -p ${LOG_LEVEL} -t randomly "${MESSAGE}"
+}
+
+NUMBER=$RANDOM
+echo "${NUMBER}"
+logit user.info ${NUMBER}
+
+# The following works on MacOS, where looking in /var/log/messages or /var/log/sysinfo does not.
+# MacOS also seems to ignore -i (always including the PID) and -t (never including the tag).
+log show --predicate "eventMessage contains '${NUMBER}'" --last 10m --info
