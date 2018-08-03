4c6053c5ca6d73be93fc86db68035f7e8ccc9254
Franklin Wu <franklin.w.wu@gmail.com>
2018-08-03 02:53:49 -0700

    Complete exercises; complete course

--- exercises/10-debugging/exercise-01.sh
+++ exercises/10-debugging/exercise-01.sh
@@ -1,6 +1,18 @@
-#! /bin/bash
+#! /bin/bash -e -x
 
 # Write a shell script that exit on error and displays commands as they will execute, including all
 # expansions and substitutions. Use 3 ls commands in your script. Make the first one succeed,
 # the second one fail, and the third one succeed. If you are using the proper options, the third ls
 # command will not be executed.
+
+OPTIONS="al"
+ls -"${OPTIONS}"
+echo
+
+OPTIONS="z"
+ls -"${OPTIONS}"
+echo
+
+OPTIONS="1"
+ls -"${OPTIONS}"
+
--- exercises/10-debugging/exercise-02.sh
+++ exercises/10-debugging/exercise-02.sh
@@ -1,4 +1,16 @@
-#! /bin/bash
+#! /bin/bash -x
 
 # Modify the previous exercise so that script continues, even if an error occurs. This time all three
 # ls commands will execute.
+
+OPTIONS="al"
+ls -"${OPTIONS}"
+echo
+
+OPTIONS="z"
+ls -"${OPTIONS}"
+echo
+
+OPTIONS="1"
+ls -"${OPTIONS}"
+
